2f983cdcec50f5979f8184ee9910ba8084a1dfff
Mahmut Akyol <mahmut.akyol@outlook.com.tr>
2015-07-31 12:14:20 +0300

    fileUpload.php
    file upload

---
+++ fileUpload.php
@@ -0,0 +1,28 @@
+<?php
+
+$target_dir = "../files/media";
+
+if (!file_exists($target_dir)) {
+    mkdir($target_dir, 0777, true);
+}
+
+$fileName = RandomString();
+$originalFileName = $_FILES["photo"]["name"];
+$target_dir = $target_dir . "/" . $fileName . ".jpg"; //you can use .jpg or whatever mimetype
+
+if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_dir)) {
+    $query = $db->query("
+        INSERT INTO table_names (`type`, category, description, `date`, `value`, attachment, attachment_description)
+        VALUES ('{$type}', '{$category}', '{$description}', '{$date}',  '{$value}', '{$fileName}', '{$originalFileName}')
+    ");
+    if ($query) {
+        $json["status"] = "ok";
+    } else {
+        $json["status"] = "uuppps somethings wrong please check the database connections";
+    }
+} else {
+    $json["status"] = "Sorry, there was an error uploading your file";
+}
+echo json_encode($json);
+
+?>
